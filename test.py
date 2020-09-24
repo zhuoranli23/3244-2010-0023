5ff748c044810415e3968ef4c1629f50d0daff27
xinming <xinming.zh@gmail.com>
2020-09-24 23:02:41 +0800

    fix windows

--- test.py
+++ test.py
@@ -9,72 +9,77 @@ import utils
 from discriminators import define_Dis
 from generators import define_Gen
 
-crop_height = 128
-crop_width = 128
-dataset_dir = './datasets/sketch2pokemon'
-checkpoint_dir = './checkpoints/sketch2pokemon'
-checkpoint_name = 'latest'
-results_dir = './results'
-batch_size = 1
-ngf = 64
-norm = 'instance'
-no_dropout = False
-gpu_ids = [0]
+def main():
+    crop_height = 64
+    crop_width = 64
+    dataset_dir = './datasets/animesketch2color'
+    checkpoint_dir = './checkpoints/animesketch2color'
+    checkpoint_name = 'latest'
+    results_dir = './results'
+    batch_size = 1
+    ngf = 64
+    norm = 'instance'
+    no_dropout = False
+    gpu_ids = [0]
 
-transform = transforms.Compose(
-    [transforms.Resize((crop_height,crop_width)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+    transform = transforms.Compose(
+        [transforms.Resize((crop_height, crop_width)),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
-dataset_dirs = utils.get_testdata_link(dataset_dir)
+    dataset_dirs = utils.get_testdata_link(dataset_dir)
 
-a_test_data = dsets.ImageFolder(dataset_dirs['testA'], transform=transform)
-b_test_data = dsets.ImageFolder(dataset_dirs['testB'], transform=transform)
+    a_test_data = dsets.ImageFolder(dataset_dirs['testA'], transform=transform)
+    b_test_data = dsets.ImageFolder(dataset_dirs['testB'], transform=transform)
 
+    a_test_loader = torch.utils.data.DataLoader(a_test_data, batch_size=batch_size, shuffle=False,
+                                                num_workers=4)  # set shuffle to false for testing
+    b_test_loader = torch.utils.data.DataLoader(b_test_data, batch_size=batch_size, shuffle=False, num_workers=4)
 
-a_test_loader = torch.utils.data.DataLoader(a_test_data, batch_size=batch_size, shuffle=False, num_workers=4) # set shuffle to false for testing
-b_test_loader = torch.utils.data.DataLoader(b_test_data, batch_size=batch_size, shuffle=False, num_workers=4)
+    Gab = define_Gen(input_nc=3, output_nc=3, ngf=ngf, netG='resnet_9blocks', norm=norm,
+                     use_dropout=not no_dropout, gpu_ids=gpu_ids)
+    Gba = define_Gen(input_nc=3, output_nc=3, ngf=ngf, netG='resnet_9blocks', norm=norm,
+                     use_dropout=not no_dropout, gpu_ids=gpu_ids)
 
-Gab = define_Gen(input_nc=3, output_nc=3, ngf=ngf, netG='resnet_9blocks', norm=norm, 
-                                                use_dropout= not no_dropout, gpu_ids=gpu_ids)
-Gba = define_Gen(input_nc=3, output_nc=3, ngf=ngf, netG='resnet_9blocks', norm=norm, 
-                                                use_dropout= not no_dropout, gpu_ids=gpu_ids)
+    utils.print_networks([Gab, Gba], ['Gab', 'Gba'])
 
-utils.print_networks([Gab,Gba], ['Gab','Gba'])
+    try:
+        ckpt = utils.load_checkpoint('%s/%s.ckpt' % (checkpoint_dir, checkpoint_name))
+        Gab.load_state_dict(ckpt['Gab'])
+        Gba.load_state_dict(ckpt['Gba'])
+    except:
+        print(' [*] No checkpoint!')
 
-try:
-    ckpt = utils.load_checkpoint('%s/%s.ckpt' % (checkpoint_dir, checkpoint_name))
-    Gab.load_state_dict(ckpt['Gab'])
-    Gba.load_state_dict(ckpt['Gba'])
-except:
-    print(' [*] No checkpoint!')
+    def save_sample_image(len):
+        itera = iter(a_test_loader)
+        iterb = iter(b_test_loader)
+        res = []
+        for i in range(len):
+            a = itera.next()
+            b = iterb.next()
 
-def save_sample_image(len):
-    itera = iter(a_test_loader)
-    iterb = iter(b_test_loader)
-    res = []
-    for i in range(len):
-        a = itera.next()
-        b = iterb.next()
-        a_real_test = Variable(a[0], requires_grad=True)
-        b_real_test = Variable(b[0], requires_grad=True)
-        a_real_test, b_real_test = utils.cuda([a_real_test, b_real_test])
-        Gab.eval()
-        Gba.eval()
+            a_real_test = Variable(a[0], requires_grad=True)
+            b_real_test = Variable(b[0], requires_grad=True)
+            a_real_test, b_real_test = utils.cuda([a_real_test, b_real_test])
+            Gab.eval()
+            Gba.eval()
 
-        with torch.no_grad():
-            a_fake_test = Gab(b_real_test)
-            b_fake_test = Gba(a_real_test)
-            a_recon_test = Gab(b_fake_test)
-            b_recon_test = Gba(a_fake_test)
+            with torch.no_grad():
+                a_fake_test = Gab(b_real_test)
+                b_fake_test = Gba(a_real_test)
+                a_recon_test = Gab(b_fake_test)
+                b_recon_test = Gba(a_fake_test)
 
-            res.extend([a_real_test, b_fake_test, b_real_test])
+                res.extend([a_real_test, b_fake_test, b_real_test])
 
-    pic = (torch.cat(res, dim=0).data + 1) / 2.0
+        pic = (torch.cat(res, dim=0).data + 1) / 2.0
 
-    if not os.path.isdir(results_dir):
-        os.makedirs(results_dir)
+        if not os.path.isdir(results_dir):
+            os.makedirs(results_dir)
 
-    torchvision.utils.save_image(pic, results_dir+'/result_%d.jpg' % ckpt['epoch'], nrow=6)
+        torchvision.utils.save_image(pic, results_dir + '/result_%d.jpg' % ckpt['epoch'], nrow=6)
 
-save_sample_image(8)
+    save_sample_image(8)
+
+if __name__ == '__main__':
+    main()
