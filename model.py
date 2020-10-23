d360aae74aca6930aa0f5d2b607070a2ec7fd850
Tan Yuanhong <leotan.yh@gmail.com>
2020-10-24 00:19:47 +0800

    Add hist metrics and stability measure

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import itertools
 import functools
 
 import os
+import numpy as np
+import cv2
 import torch
 import torchvision
 from torch import nn
@@ -77,11 +79,20 @@ class cycleGAN(object):
         # Tensorboard Setup
         self.writer = SummaryWriter('logs/pokemon_coloring')
 
+        # Stability variables setup
+        self.last_test_output = []
+        self.cur_test_output = []
+
     def save_sample_image(self, test_length, a_test_loader, b_test_loader, results_dir, epoch):
+        temp_dir = './temp'
         itera = iter(a_test_loader)
         iterb = iter(b_test_loader)
         res = []
-        for i in range(test_length):
+        average_hist_score_correlation = 0.0
+        average_hist_score_chi_square = 0.0
+        average_hist_score_intersction = 0.0
+        average_hist_score_bhat = 0.0
+        for _ in range(test_length):
             a = itera.next()
             b = iterb.next()
             a_real_test = Variable(a[0], requires_grad=True)
@@ -91,18 +102,65 @@ class cycleGAN(object):
             self.Gba.eval()
 
             with torch.no_grad():
-                a_fake_test = self.Gab(b_real_test)
+                # a_fake_test = self.Gab(b_real_test)
                 b_fake_test = self.Gba(a_real_test)
-                a_recon_test = self.Gab(b_fake_test)
-                b_recon_test = self.Gba(a_fake_test)
+                # a_recon_test = self.Gab(b_fake_test)
+                # b_recon_test = self.Gba(a_fake_test)
 
                 res.extend([a_real_test, b_fake_test, b_real_test])
+                self.cur_test_output.append(b_fake_test)
+                
+                ##### Generate histogram similarity score
+                # Documentation: https://docs.opencv.org/3.4/d8/dc8/tutorial_histogram_comparison.html
+                torchvision.utils.save_image(b_fake_test.cpu(), temp_dir + '/model_output.jpg')
+                torchvision.utils.save_image(b_real_test.cpu(), temp_dir + '/groundtruth.jpg')
+                src_base = cv2.imread(temp_dir + '/groundtruth.jpg')
+                src_test1 = cv2.imread(temp_dir + '/model_output.jpg')
+                hsv_base = cv2.cvtColor(src_base, cv2.COLOR_BGR2HSV)
+                hsv_test1 = cv2.cvtColor(src_test1, cv2.COLOR_BGR2HSV)
+                h_bins = 50
+                s_bins = 60
+                v_bins = 60
+                histSize = [h_bins, s_bins, v_bins]
+                # hue varies from 0 to 179, saturation from 0 to 255, value from 0 to 255
+                h_ranges = [0, 180]
+                s_ranges = [0, 256]
+                v_ranges = [0, 256]
+                ranges = h_ranges + s_ranges + v_ranges# concat lists
+                # Use the 0-th and 1-st channels
+                channels = [0, 1, 2]
+                hist_base = cv2.calcHist([hsv_base], channels, None, histSize, ranges, accumulate=False)
+                cv2.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+                hist_test1 = cv2.calcHist([hsv_test1], channels, None, histSize, ranges, accumulate=False)
+                cv2.normalize(hist_test1, hist_test1, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+                # compare_method = {0: 'correlation', 1: 'chi-square', 2: 'intersection', 3: 'Bhattacharyya'}
+                average_hist_score_correlation += cv2.compareHist(hist_base, hist_test1, 0)
+                average_hist_score_chi_square += cv2.compareHist(hist_base, hist_test1, 1)
+                average_hist_score_intersction += cv2.compareHist(hist_base, hist_test1, 2)
+                average_hist_score_bhat += cv2.compareHist(hist_base, hist_test1, 3)
+                # os.remove(temp_dir + '/model_output.jpg')
+                # os.remove(temp_dir + '/groundtruth.jpg')
+        self.writer.add_scalar('HistCompare_correlation', average_hist_score_correlation / test_length, epoch)
+        self.writer.add_scalar('HistCompare_chi_square', average_hist_score_chi_square / test_length, epoch)
+        self.writer.add_scalar('HistCompare_intersction', average_hist_score_intersction / test_length, epoch)
+        self.writer.add_scalar('HistCompare_bhat', average_hist_score_bhat / test_length, epoch)
+        
+        if len(self.last_test_output) > 0:
+            _sum = 0.0
+            for i in range(test_length):
+                _sum += np.linalg.norm(np.abs(self.last_test_output[i].cpu() - self.cur_test_output[i].cpu()))
+            self.writer.add_scalar('L1 dist against last epoch', _sum / test_length, epoch)
+        self.last_test_output = []
+        for item in self.cur_test_output:
+            self.last_test_output.append(item)
+        self.cur_test_output = []
+
 
         pic = (torch.cat(res, dim=0).data + 1) / 2.0
 
         if not os.path.isdir(results_dir):
             os.makedirs(results_dir)
-        self.writer.add_image(f'testing_epoch_{epoch}', pic, epoch, dataformats='NCHW')
+        self.writer.add_image(f'test_image', pic, epoch, dataformats='NCHW')
         torchvision.utils.save_image(pic, results_dir + '/result_%d.jpg' % epoch, nrow=6)
 
     def train(self, args):
@@ -259,16 +317,6 @@ class cycleGAN(object):
             self.writer.add_scalar('Dis_LR', self.d_lr_scheduler.get_lr()[0], epoch)
             # Override the latest checkpoint
             #######################################################
-            '''
-            utils.save_checkpoint({'epoch': epoch + 1,
-                                   'Da': self.Da.state_dict(),
-                                   'Db': self.Db.state_dict(),
-                                   'Gab': self.Gab.state_dict(),
-                                   'Gba': self.Gba.state_dict(),
-                                   'd_optimizer': self.d_optimizer.state_dict(),
-                                   'g_optimizer': self.g_optimizer.state_dict()},
-                                  '%s/epoch %d.ckpt' % (args.checkpoint_dir, epoch+1))
-            '''
             utils.save_checkpoint({'epoch': epoch + 1,
                                    'Da': self.Da.state_dict(),
                                    'Db': self.Db.state_dict(),
